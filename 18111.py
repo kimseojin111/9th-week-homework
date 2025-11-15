24cc47b3a52fd8bc4dddd34ced363d5beffd2ff4
gs25 <root@jyAckerman.>
2025-11-15 14:39:02 +0900

    comment

--- 18111.py
+++ 18111.py
@@ -1,13 +1,22 @@
 import sys 
 lambda input : sys.stdin.readline 
 
+'''
+관찰 : 정답이 되는 과정은 무조건 존재한다. -> 모든 높이를 0으로 맞추면 된다. 
+정답이 될 높이는 0이상 256이하이다. -> 256 보다 크게 만드는 것은 불필요하다. 
+정답이 될 높이를 h로 고정시키자. 그러면 먼저 h보다 큰 애들을 모두 h로 맞춰준다. 
+이때 남은 인벤토리 내부 블록 개수로 h보다 작은 애들을 체워줄 수 있으면 된다. 
+'''
+
 n,m,B = map(int,input().split()) 
 arr = [] 
 for i in range(n):
     inp = list(map(int,input().split())) 
     arr += inp 
 
-ans_list = [] 
+# 그냥 높이만 필요하니 모두 flatten 시켜놓는다. 
+
+ans_list = [] # 필요한 시간, h 쌍들을 저장할 리스트 
 for h in range(0,257):
     inven = B 
     time_spent = 0 
